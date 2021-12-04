373bbe7204202515322e8cff521cb4581a8d93c0
ArzuHasanova <harzu1996@gmail.com>
2021-12-04 14:13:50 +0400

    -- add cross origin

--- src/main/java/az/ibar/ms/phonebook/controller/PhoneBookController.java
+++ src/main/java/az/ibar/ms/phonebook/controller/PhoneBookController.java
@@ -7,6 +7,7 @@ import az.ibar.ms.phonebook.entity.PhoneBookEntity;
 import az.ibar.ms.phonebook.service.PhoneBookService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +18,13 @@ import java.util.List;
 @AllArgsConstructor
 @Validated
 @RequestMapping("user")
+@CrossOrigin("*")
 public class PhoneBookController {
 
     private final PhoneBookService phoneBookService;
 
 
-    @PostMapping("add")
+    @PostMapping(value = "add")
     public ResponseEntity<PhoneBookResponseDto> addUser(@RequestBody PhoneBookDto request) {
         return ResponseEntity.ok(phoneBookService.save(request));
     }
@@ -37,7 +39,7 @@ public class PhoneBookController {
         return ResponseEntity.ok(phoneBookService.delete(userId));
     }
 
-    @GetMapping("list")
+    @GetMapping(value = "list",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<PhoneBookEntity>> getAllUsers() {
         return ResponseEntity.ok(phoneBookService.getAllUsers());
     }
